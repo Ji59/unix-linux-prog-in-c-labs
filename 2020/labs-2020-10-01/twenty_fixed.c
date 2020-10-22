04b25b1e345814309221dc79cbfc3a03fcf3e942
ji59 <jirikotal@vivaldi.net>
2020-10-22 11:00:51 +0200

    Fixed twenty.c.

---
+++ 2020/labs-2020-10-01/twenty_fixed.c
@@ -0,0 +1,18 @@
+/*
+ * The program is supposed to print twenty '-' characters.
+ * It fails to do that.
+ *
+ * Change one character to fix it.
+ */
+
+#include <stdio.h>
+
+int
+main(void) {
+	int i, n = 20;
+
+	for (i = 0; i < n; n--) {
+		printf("-");
+		putchar('\n');
+	}
+}
